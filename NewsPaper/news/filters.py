55ff470f60dfa81f9938b6275259beb7bba01242
greenman20130 <deadbread228@gmail.com>
2023-12-09 01:04:37 +0300

    update
    update search
    add delete and update post

--- NewsPaper/news/filters.py
+++ NewsPaper/news/filters.py
@@ -1,20 +1,22 @@
-from django_filters import FilterSet, DateFilter
-from .models import Post
+from django_filters import FilterSet
+from django.forms import DateInput
+from .models import Post, Category
 from django import forms
-
+import django_filters
 
 
 class PostFilter(FilterSet):
-    time = DateFilter(
-        field_name='time', 
-        widget=forms.DateInput(attrs={type:'date'}),
-        lookup_expr='date__gte'
-    )
+    title = django_filters.Filter(field_name='title', lookup_expr='icontains', label='Название')
+    time = django_filters.DateFilter(field_name='time', lookup_expr='gte', widget=DateInput(attrs={'type': 'date'}), label='С')
+    category = django_filters.ModelMultipleChoiceFilter(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple(attrs={'category': 'category'}), label = 'Категория') 
+    author = django_filters.Filter(field_name='author', lookup_expr='icontains', label='Автор')
+    text = django_filters.Filter(field_name='text', lookup_expr='icontains', label='Содержание')
     class Meta:
         model = Post
         fields = {
-            # поиск по названию
-            'title': ['icontains'],
-            'author_id__user__username': ['icontains'],
-            'time' : ['gt'],
+            'category',
+            'author',
+            'title',
+            'time',
+            'text',
         }
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .filters import PostFilter
+from .forms import PostForm
 
 class PostsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -15,6 +17,7 @@ class PostsList(ListView):
     context_object_name = 'posts'
     paginate_by = 10
 
+
     def get_queryset(self):
        # Получаем обычный запрос
        queryset = super().get_queryset()
@@ -26,6 +29,12 @@ class PostsList(ListView):
        self.filterset = PostFilter(self.request.GET, queryset)
        # Возвращаем из функции отфильтрованный список товаров
        return self.filterset.qs
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 
 class PostDetail(DetailView):
@@ -42,8 +51,44 @@ class PostSearch(ListView):
     ordering = '-time'
     template_name = 'search.html'
     context_object_name = 'posts'
+
+
     def get_queryset(self):
        queryset = super().get_queryset()
        self.filterset = PostFilter(self.request.GET, queryset)
        # Возвращаем из функции отфильтрованный список товаров
        return self.filterset.qs
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        if '/article/' in self.request.path:
+            type_ = 'AR'
+        elif '/news/' in self.request.path:
+            type_ = 'NE'
+        self.object.type = type_
+        return super().form_valid(form)
+    
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
